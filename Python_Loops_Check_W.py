6633a014bb6e8fc8c52616ef86c47f845531ff54
730443 <730443@605-110-S13>
2023-04-07 08:48:46 -0600

    Add project files.

---
+++ Python_Loops_Check_W.py
@@ -0,0 +1,33 @@
+
+for i in range(-20, 41, 5):
+    print(i)
+
+print()
+print()
+
+for i in range(2):
+    for j in range(5):
+        print("*", end = " ")
+    print()
+
+print()
+print()
+
+num = 50
+while num >= 0:
+    print(num)
+    num -= 10
+
+
+print()
+print()
+
+user = input("Here is a cookie! Do you want another?")
+while user != 'q':
+    print("Here is a cookie!")
+    user = input("Do you want another? Or type 'q' to quit")
+
+print("Ok bye!")
+   
+
+        
